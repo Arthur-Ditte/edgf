75207e7b066253a072f5b596a618ecdfb2fc00a1
RestytheCake <Morganischkalt@gmail.com>
2024-08-09 20:02:17 +0200

    trying vercel support

--- api/settings.py
+++ api/settings.py
@@ -97,7 +97,11 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
